d049156d07df6cb54f4ea3c246ddfd11947be7bd
Muslehud77 <fardinmohit@gmail.com>
2024-03-25 03:02:44 +0800

    first commit

---
+++ src/Redux/store.js
@@ -0,0 +1,34 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+const store = configureStore({
+    reducer: {}
+})
+
+export default store
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
