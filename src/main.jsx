2b4c3cdd3b3d9b3e1bb51c9554f8800fd8cf30ad
Muslehud77 <fardinmohit@gmail.com>
2024-03-25 03:22:24 +0800

    initial project setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
+
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './Redux/store.js'
+import { RouterProvider } from 'react-router-dom'
+import router from './Routes/Routes'
+
+
+
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
+   <RouterProvider router={router}/>
   </Provider>
 );
